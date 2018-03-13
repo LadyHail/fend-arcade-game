7e10a9eecc9df86af6f34733ce294dc3321e4400
LadyHail <ladydeath@o2.pl>
2018-03-13 12:52:24 +0100

    Characters render and move properly

--- js/app.js
+++ js/app.js
@@ -1,36 +1,81 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+const OFFSCREEN_X = 505;
+const OFFSCREEN_Y = 606;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+class Character {
+    constructor(posX = 0, posY = 0, speed = 100, sprite) {
+        this.posX = posX;
+        this.posY = posY;
+        this.speed = speed;
+        this.sprite = sprite;
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    update(dt) {
+        this.posX += this.speed * dt;
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.posX, this.posY);
+    }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+    getLocation() {
+        return [this.posX, this.posY];
+    }
+}
 
+class Enemy extends Character {
+    constructor(posX, posY, speed, sprite = 'images/enemy-bug.png') {
+        super(posX, posY, speed, sprite);
+    }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+    update(dt) {
+        super.update(dt);
+        if (this.posX >= OFFSCREEN_X) {
+            this.posX = -100;
+        }
+    }
 
+    render() {
+        super.render();
+    }
+}
 
+class Player extends Character {
+    constructor(posX, posY, speed, sprite = 'images/char-boy.png') {
+        super(posX, posY, speed, sprite);
+        this.MOVE_POS_X = 101;
+        this.MOVE_POS_Y = 83;
+    }
+
+    update() {}
+
+    render() {
+        super.render();
+    }
+
+    handleInput(keyCode) {
+        switch (keyCode) {
+            case 'left':
+                this.posX -= this.MOVE_POS_X;
+                break;
+
+            case 'right':
+                this.posX += this.MOVE_POS_X;
+                break;
+
+            case 'up':
+                this.posY -= this.MOVE_POS_Y;
+                break;
+
+            case 'down':
+                this.posY += this.MOVE_POS_Y;
+                break;
+        }
+    }
+}
+
+const player = new Player(202, 375);
+const allEnemies = new Array();
+allEnemies.push(new Enemy(0, 60), new Enemy(202, 145), new Enemy(404,230));
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
