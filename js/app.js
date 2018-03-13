2ad30b321acf7289ab51d7131789e5cdd2a19f69
LadyHail <ladydeath@o2.pl>
2018-03-13 20:17:38 +0100

    Add collision player - enemy

--- js/app.js
+++ js/app.js
@@ -23,12 +23,20 @@ class Character {
 }
 
 class Enemy extends Character {
+    static get SIZE_X() {
+        return 60;
+    }
+
+    static get SIZE_Y() {
+        return 60;
+    }
     constructor(posX, posY, speed, sprite = 'images/enemy-bug.png') {
         super(posX, posY, speed, sprite);
     }
 
     update(dt) {
         super.update(dt);
+        this.checkCollision();
         if (this.posX >= OFFSCREEN_X) {
             this.posX = -100;
         }
@@ -37,13 +45,46 @@ class Enemy extends Character {
     render() {
         super.render();
     }
+
+    checkCollision() {
+        const enemyHitbox = { posX: this.posX, posY: this.posY, size_X: Enemy.SIZE_X, size_Y: Enemy.SIZE_Y };
+        const playerHitbox = { posX: player.posX, posY: player.posY, size_X: Player.SIZE_X, size_Y: Player.SIZE_Y };
+
+        if (playerHitbox.posX < enemyHitbox.posX + enemyHitbox.size_X &&
+            playerHitbox.posX + playerHitbox.size_X > enemyHitbox.posX &&
+            playerHitbox.posY < enemyHitbox.posY + enemyHitbox.size_Y &&
+            playerHitbox.posY + playerHitbox.size_Y > enemyHitbox.posY) {
+            player.collision('enemy');
+        }
+    }
 }
 
 class Player extends Character {
-    constructor(posX, posY, speed, sprite = 'images/char-boy.png') {
+    static get MOVE_POS_X() {
+        return 101;
+    }
+
+    static get MOVE_POS_Y() {
+        return 83;
+    }
+
+    static get SIZE_X() {
+        return 50;
+    }
+
+    static get SIZE_Y() {
+        return 22; // minimum value to correct collision detection
+    }
+
+    static get START_X() {
+        return 202;
+    }
+
+    static get START_Y() {
+        return 375;
+    }
+    constructor(posX = 202, posY = 375, speed, sprite = 'images/char-boy.png') {
         super(posX, posY, speed, sprite);
-        this.MOVE_POS_X = 101;
-        this.MOVE_POS_Y = 83;
     }
 
     update() {}
@@ -55,25 +96,38 @@ class Player extends Character {
     handleInput(keyCode) {
         switch (keyCode) {
             case 'left':
-                this.posX -= this.MOVE_POS_X;
+                this.posX -= Player.MOVE_POS_X;
                 break;
 
             case 'right':
-                this.posX += this.MOVE_POS_X;
+                this.posX += Player.MOVE_POS_X;
                 break;
 
             case 'up':
-                this.posY -= this.MOVE_POS_Y;
+                this.posY -= Player.MOVE_POS_Y;
                 break;
 
             case 'down':
-                this.posY += this.MOVE_POS_Y;
+                this.posY += Player.MOVE_POS_Y;
                 break;
         }
     }
+
+    collision(target) {
+        switch (target) {
+            case 'enemy':
+                this.reset();
+                break;
+        }
+    }
+
+    reset() {
+        this.posX = Player.START_X;
+        this.posY = Player.START_Y;
+    }
 }
 
-const player = new Player(202, 375);
+const player = new Player();
 const allEnemies = new Array();
 allEnemies.push(new Enemy(0, 60), new Enemy(202, 145), new Enemy(404,230));
 
