93812f42bc3e4db68108fd3eef43bd5eb6acba5b
LadyHail <ladydeath@o2.pl>
2018-03-14 21:48:21 +0100

    Add comments to JS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+// Maximum value on x-axis
 const OFFSCREEN_X = 505;
+
+// Maximum value on y-axis
 const OFFSCREEN_Y = 606;
+
+// Value to use in collision detection. Minimum to work collision properly.
 const COLLIDER = 60;
+
 let playerHitbox = new Object();
 
 class Character {
@@ -11,10 +17,12 @@ class Character {
         this.sprite = sprite;
     }
 
+    // Update character position depending on time and speed
     update(dt) {
         this.posX += this.speed * dt;
     }
 
+    // Render sprites for each character.
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.posX, this.posY);
     }
@@ -29,6 +37,8 @@ class Enemy extends Character {
     update(dt) {
         super.update(dt);
         player.checkCollision(this);
+
+        // Reset enemy position if it goes off-screen
         if (this.posX >= OFFSCREEN_X) {
             this.posX = -100;
         }
@@ -42,6 +52,7 @@ class Enemy extends Character {
         return Math.round(Math.random() * 100 + Math.random() * 10);
     }
 
+    // Get new enemies spwning on random location with random speed. Amount of enemies depends on level.
     static generateNewEnemies() {
         allEnemies = [];
         if (ui.level <= 3) {
@@ -57,6 +68,8 @@ class Enemy extends Character {
 }
 
 class Player extends Character {
+
+    // Values represents one step by player on x and y-axis 
     static get MOVE_POS_X() {
         return 101;
     }
@@ -65,6 +78,7 @@ class Player extends Character {
         return 83;
     }
 
+    // Values needed to collision detection. Represent players width(x) and height(y)
     static get SIZE_X() {
         return 50;
     }
@@ -73,6 +87,7 @@ class Player extends Character {
         return 22; // minimum value to correct collision detection
     }
 
+    // Starting player position
     static get START_X() {
         return 202;
     }
@@ -80,10 +95,12 @@ class Player extends Character {
     static get START_Y() {
         return 375;
     }
+
     constructor(posX = 202, posY = 375, speed, sprite = 'images/char-boy.png') {
         super(posX, posY, speed, sprite);
     }
 
+    // Every update calculate player hitbox (collision detection)
     update() {
         playerHitbox = { posX: player.posX, posY: player.posY, size_X: Player.SIZE_X, size_Y: Player.SIZE_Y };
     }
@@ -92,6 +109,7 @@ class Player extends Character {
         super.render();
     }
 
+    // Move player in correct direction. Depends on player input key
     handleInput(keyCode) {
         switch (keyCode) {
             case 'left':
@@ -113,6 +131,7 @@ class Player extends Character {
         }
     }
 
+    // Collision detection based on: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
     checkCollision(obj) {
         const objectHitbox = { posX: obj.posX, posY: obj.posY, collider: COLLIDER };
 
@@ -124,6 +143,7 @@ class Player extends Character {
         }
     }
 
+    // If collision is detected, players behaviour depends on object the detection was made.
     collision(obj) {
         switch (obj.tag) {
             case 'enemy':
@@ -143,6 +163,7 @@ class Player extends Character {
         }
     }
 
+    // Reset player to starting position
     reset() {
         this.posX = Player.START_X;
         this.posY = Player.START_Y;
@@ -150,16 +171,17 @@ class Player extends Character {
 
     win() {
         if (player.posY < 0) {
-            allEnemies = [];
+            allEnemies = []; // need to be clear before player can get reset()
             this.reset();
             ui.addScore(1000);
-            setTimeout(function () { //to prevent player from loosing live
+            setTimeout(function () { // BUG FIXED: sometimes player lost live after reset - detected collision while reseting player
                 ui.addLevel();
             }, 10);
         }
     }
 }
 
+// The class represents collectables. Can be used for other types of objects.
 class GameObject {
     constructor(tag, posX, posY, sprite) {
         this.tag = tag;
@@ -176,6 +198,7 @@ class GameObject {
         ctx.drawImage(Resources.get(this.sprite), this.posX, this.posY);
     }
 
+    // Generate objects on new positions.
     static generateNewObjects() {
         gameObjects.forEach(function (obj) {
             let newObj = new GameObject(obj.tag, randomPosition()[0], randomPosition()[1], obj.sprite);
@@ -184,6 +207,7 @@ class GameObject {
     }
 }
 
+// The class represents user interface.
 class UI {
     constructor() {
         this.lives = 3;
@@ -196,6 +220,7 @@ class UI {
         this.drawLevel();
     }
 
+    // Draw hearts as representation of lives
     drawLives() {
         let posX = 0;
         for (var i = 1; i <= this.lives; i++) {
@@ -204,12 +229,14 @@ class UI {
         }
     }
 
+    // Display score
     drawScore() {
         ctx.font = '30px Arial';
         ctx.textAlign = 'right';
         ctx.fillText('Score: ' + this.score, 500, OFFSCREEN_Y - 30);
     }
 
+    // Display level
     drawLevel() {
         ctx.font = '24px Arial';
         ctx.textAlign = 'right';
@@ -242,11 +269,14 @@ class UI {
     }
 
     gameOver() {
-        alert('Game Over');
+        alert('Game Over\nScore: ' + this.score);
         location = location;
     }
 }
 
+// Returns randomly generated position. 
+// Possible positions on x: 0, 101, 202, 303, 404.
+// Possible positions on y: 60, 145, 230.
 function randomPosition() {
     const posX = 101 * Math.floor(Math.random() * 4);
     const posY = 60 + (85 * Math.floor(Math.random() * 3));
@@ -263,8 +293,7 @@ let star = new GameObject('star', randomPosition()[0], randomPosition()[1], 'ima
 const gameObjects = new Array();
 gameObjects.push(heart, star);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
